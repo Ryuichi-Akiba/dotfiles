f60bda854dd30a7ceb6fbddf213d8efa7a870edc
Ryuichi Akiba <matsu6bei@gmail.com>
2014-04-27 00:18:45 +0900

    行ハイライトの設定追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,6 +36,26 @@
  ;; 英語名の場合
  ;; (font-spec :family "Hiragino Mincho Pro"))
  (font-spec :family "ヒラギノ丸ゴ Pro"))
+;; フォントの横幅を調整
+(setq face-font-rescale-alist
+      '((".*Menlo.*" . 1.0)
+        (".*Hiragino_Mincho_Pro.*" . 1.2)
+        (".*Hiragino_Maru_Gothic_Pro.*" . 1.2)
+        (".*Hiragino_Kaku_Gothic_Pro.*" . 1.2)
+        ("-cdac$" . 1.3)))
+
+;; 現在行のハイライト
+(defface my-hl-line-face
+  ;; 背景がdarkならば背景色を紺に
+  '((((class color) (background dark))
+     (:background "NavyBlue" t))
+    ;; 背景がlightならば背景色を緑に
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-face)
+(global-hl-line-mode t)
 
 ;; TABの表示幅。初期値は8
 (setq-default tab-width 4)
@@ -46,10 +66,19 @@
   ;; テーマを読み込むための設定
   (color-theme-initialize)
   ;; テーマを変更する
-  (color-theme-subtle-hacker))
+  (color-theme-deep-blue))
 
 ;; リージョンの背景色を変更
 (set-face-background 'region "darkgreen")
+
+;; paren-mode:対応する括弧を強調して表示する
+(setq show-paren-delay 0)
+(show-paren-mode t)
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
 
 ;; タイトルバーにファイルのフルパスを表示
 (setq frame-title-format "%f")
