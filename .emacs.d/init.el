138f04ccdc1fc178f9c6a93f816c4ccb6ea95c0d
Ryuichi Akiba <matsu6bei@gmail.com>
2014-04-27 11:10:44 +0900

    バックアップとオートセーブファイルの保存場所を変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -105,6 +105,16 @@
 ;; オートセーブファイルの作成場所をシステムのTempディレクトリに変更する
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+;; オートセーブファイル作成までの秒間隔
+;; (setq auto-save-timeout 15)
+;; オートセーブファイル作成までのタイプ間隔
+;; (setq auto-save-interval 60)
+
 
 ;; 入力されるシーケンスを置き換える
 ;; ?\C-? はDELのシーケンス
